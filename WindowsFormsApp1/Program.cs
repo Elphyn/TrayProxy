6c647ef90fda092dc75a63a2b6d5e826a64aae42
Elphyn <vladkarelov14@gmail.com>
2023-03-21 20:08:19 +0900

    Release

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -35,6 +35,9 @@ namespace WindowsFormsApp1
                 ContextMenu = new ContextMenu(new MenuItem[] {
                 new MenuItem("Proxy On", On),
                 new MenuItem("Proxy Off", Off),
+                new MenuItem("", Check),
+                new MenuItem("Change to USA", ChangeProxyUSA ),
+                new MenuItem("Change to KZ", ChangeProxyKz),
                 new MenuItem("Exit", Exit)
             }),
                 Visible = true
@@ -63,6 +66,40 @@ namespace WindowsFormsApp1
             trayIcon.Visible = false;
 
             Application.Exit();
+        }
+        void Check(object sender, EventArgs e)
+        {
+            const string userRoot = "HKEY_CURRENT_USER";
+            const string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+            const string keyName = userRoot + "\\" + subKey;
+
+
+        }
+        void ChangeProxyUSA(object sender, EventArgs e)
+        {
+            const string ipAddress = "154.13.35.170";
+            const string portNumber = "63556";
+            // Get the path to the Internet Settings registry key
+            const string registryKeyPath = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+
+            // Set the proxy server IP and port number values
+            Registry.SetValue(registryKeyPath, "ProxyServer", ipAddress + ":" + portNumber, RegistryValueKind.String);
+
+            // Enable proxy settings
+            Registry.SetValue(registryKeyPath, "ProxyEnable", 1, RegistryValueKind.DWord);
+        }
+        void ChangeProxyKz(object sender, EventArgs e)
+        {
+            const string ipAddress = "193.107.22.60";
+            const string portNumber = "56799";
+            // Get the path to the Internet Settings registry key
+            const string registryKeyPath = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+
+            // Set the proxy server IP and port number values
+            Registry.SetValue(registryKeyPath, "ProxyServer", ipAddress + ":" + portNumber, RegistryValueKind.String);
+
+            // Enable proxy settings
+            Registry.SetValue(registryKeyPath, "ProxyEnable", 1, RegistryValueKind.DWord);
         }
     }
 }
